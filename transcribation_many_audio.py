e035d58e53c2908ff57008ddffe7d093e39dce77
Evgeniy Parygin <evgeniy.parygin@yandex.ru>
2021-02-24 13:23:21 +0300

    Minor bugs fixed

--- transcribation_many_audio.py
+++ transcribation_many_audio.py
@@ -11,11 +11,10 @@ FILE = 'links_list.txt'
 FOLDER = 'results'
 BUCKET = os.getenv('BUCKET')
 FILE_LINK = 'https://storage.yandexcloud.net/{bucket}/{file_key}'
-API = 'https://transcribe.api.cloud.yandex.net/speech/stt/{VERSION}/{METHOD}'
-VERSION = 'v2'
+API = 'https://transcribe.api.cloud.yandex.net/speech/stt/v2/{method}'
 METHOD = 'longRunningRecognize'
 RESULT_URL = 'https://operation.api.cloud.yandex.net/operations/{request_id}'
-TIME_SLEEP = 5
+TIME_SLEEP = 30
 TIMEOUT = 30
 
 logging.basicConfig(
@@ -39,8 +38,8 @@ def main():
           'Всё готово? Начинаем? [y/n]')
     start = input()
     if start == 'y':
-        processing_lines_of_the_file()
         logging.debug('Запуск скриптовой части подтверждён')
+        processing_lines_of_the_file()
     else:
         logging.debug('Выполнение скриптовой части отклоненно пользователем')
 
@@ -54,8 +53,10 @@ def processing_lines_of_the_file():
                 logging.debug(f'{one_audio} получен в обработку')
                 title = f'{FOLDER}/{one_audio.split(".")[0]}.txt'
                 request_id = send_transcription_request(one_audio)
+                logging.debug(f'{one_audio} - переход к получению текста')
                 getting_the_transcription_result(request_id, title)
                 logging.debug(f'{one_audio} полностью обработан')
+                print(f'{title} готов.')
         logging.debug('Все аудио обработаны')
         print('Работа спринта завершена, все аудио обработаны.')
     except Exception as e:
@@ -78,17 +79,16 @@ def send_transcription_request(one_audio):
         }
         try:
             req = requests.post(
-                API.format(version=VERSION, method=METHOD),
+                API.format(method=METHOD),
                 headers=header,
                 json=body,
                 timeout=TIMEOUT
             ).json()
+            logging.debug(f'ID: {req["id"]}')
+            return req['id']
         except KeyError as e:
             message = f'Ошибка парссинга json send_request. {e}'
-            logging.error(message)
-            return message
-        else:
-            return req['id']
+            logging_and_print_error_message(message)
     except ConnectionError as e:
         message = f'Не удалось отправить запрос на транскрибацию. {e}'
         logging_and_print_error_message(message)
@@ -96,26 +96,28 @@ def send_transcription_request(one_audio):
 
 def getting_the_transcription_result(request_id, title):
     try:
+        i = 1
         while True:
             time.sleep(TIME_SLEEP)
+            logging.debug(f'iteration {i}')
+            i += 1
             header = {'Authorization': 'Api-Key {}'.format(KEY)}
             try:
                 req = requests.get(
-                    RESULT_URL.format(id=request_id),
+                    RESULT_URL.format(request_id=request_id),
                     headers=header,
                     timeout=TIMEOUT
-                ).json
-            except KeyError as e:
-                message = f'Ошибка парссинга json getting_result. {e}'
-                logging.error(message)
-                return message
-            else:
+                ).json()
                 if req['done']:
                     break
+                logging.debug(f'result iteration {i} - not ready.')
+            except KeyError as e:
+                message = f'Ошибка парссинга json getting_result. {e}'
+                logging_and_print_error_message(message)
         logging.debug('Получен ответ-транскрипт аудио.')
-        with open(title, 'wb') as new_file:
+        with open(title, 'w', encoding='utf8') as new_file:
             for chunk in req['response']['chunks']:
-                new_file.write(chunk['alternatives'][0]['text'])
+                new_file.write(chunk['alternatives'][0]['text'] + '\n')
         logging.debug(f'{title} успешно сохранён')
     except ConnectionError as e:
         message = f'Не удалось получить ответ о транскрибации. {e}'
